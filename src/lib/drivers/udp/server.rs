64a8b9ce51fbb72e82d911280076ca23fe3cedc3
João Antônio Cardoso <joao.maker@gmail.com>
2026-06-10 20:50:55 -0300

    src: lib: drivers: Simplify return

--- src/lib/drivers/udp/server.rs
+++ src/lib/drivers/udp/server.rs
@@ -148,7 +148,7 @@ impl Driver for UdpServer {
 
     #[instrument(level = "debug", skip(self))]
     fn info(&self) -> Box<dyn DriverInfo> {
-        return Box::new(UdpServerInfo);
+        Box::new(UdpServerInfo)
     }
 
     fn name(&self) -> Arc<String> {
